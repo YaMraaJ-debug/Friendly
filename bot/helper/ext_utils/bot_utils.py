c3d0f4b57e67f3a3ee27d23de1d0d5213d659f98
S M Mahabub Hossain <77372461+priiiiyo@users.noreply.github.com>
2022-07-06 21:50:26 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -1,5 +1,6 @@
 from re import match as re_match, findall as re_findall
 from threading import Thread, Event
+from pytz import timezone
 from time import time
 from math import ceil
 from html import escape
@@ -8,7 +9,7 @@ from requests import head as rhead
 from urllib.request import urlopen
 from telegram import InlineKeyboardMarkup
 
-from bot import download_dict, download_dict_lock, STATUS_LIMIT, botStartTime, DOWNLOAD_DIR
+from bot import botStartTime, download_dict, download_dict_lock, STATUS_LIMIT, TIMEZONE, botStartTime, DOWNLOAD_DIR
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
@@ -21,17 +22,17 @@ PAGE_NO = 1
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading...📤"
-    STATUS_DOWNLOADING = "Downloading...📥"
-    STATUS_CLONING = "Cloning...♻️"
-    STATUS_WAITING = "Queued...💤"
-    STATUS_FAILED = "Failed 🚫. Cleaning Download..."
-    STATUS_PAUSE = "Paused...⛔️"
-    STATUS_ARCHIVING = "Archiving...🔐"
-    STATUS_EXTRACTING = "Extracting...📂"
-    STATUS_SPLITTING = "Splitting...✂️"
-    STATUS_CHECKING = "CheckingUp...📝"
-    STATUS_SEEDING = "Seeding...🌧"
+    STATUS_UPLOADING = "𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠...📥"
+    STATUS_DOWNLOADING = "𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠...📥"
+    STATUS_CLONING = "𝐂𝐥𝐨𝐧𝐢𝐧𝐠...⚙️"
+    STATUS_WAITING = "𝐐𝐮𝐞𝐮𝐞𝐝...💤"
+    STATUS_FAILED = "𝐅𝐚𝐢𝐥𝐞𝐝.🚫 𝐂𝐥𝐞𝐚𝐧𝐢𝐧𝐠 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝...📝"
+    STATUS_PAUSE = "𝐏𝐚𝐮𝐬𝐞𝐝...⛔️"
+    STATUS_ARCHIVING = "𝐀𝐫𝐜𝐡𝐢𝐯𝐢𝐧𝐠...🔐"
+    STATUS_EXTRACTING = "𝐄𝐱𝐭𝐫𝐚𝐜𝐭𝐢𝐧𝐠...📂"
+    STATUS_SPLITTING = "𝐒𝐩𝐥𝐢𝐭𝐭𝐢𝐧𝐠...✂️"
+    STATUS_CHECKING = "𝐂𝐡𝐞𝐜𝐤𝐢𝐧𝐠 𝐅𝐢𝐥𝐞...📝"
+    STATUS_SEEDING = "𝐒𝐞𝐞𝐝𝐢𝐧𝐠...🌧"
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
@@ -122,67 +123,74 @@ def get_readable_message():
                 globals()['COUNT'] -= STATUS_LIMIT
                 globals()['PAGE_NO'] -= 1
         for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
-            msg += f"<b>Name:</b> <code>{escape(str(download.name()))}</code>"
-            msg += f"\n<b>Status:</b> <i>{download.status()}</i>"
+            kie = datetime.now(pytz.timezone(f"{TIMEZONE}"))
+            jam = kie.strftime("\n 𝗗𝗮𝘁𝗲 : %d/%m/%Y\n 𝗧𝗶𝗺𝗲 : %I:%M:%S %P")
+            msg += f"{jam}"
+            msg += f"\n╭─📂 𝐅𝐢𝐥𝐞𝐧𝐚𝐦𝐞 ⇢ <code>{escape(str(download.name()))}</code>"
+            msg += f"\n├─ 𝐒𝐭𝐚𝐭𝐮𝐬 ⇢ <i>{download.status()}</i>"
             if download.status() not in [
                 MirrorStatus.STATUS_ARCHIVING,
                 MirrorStatus.STATUS_EXTRACTING,
                 MirrorStatus.STATUS_SPLITTING,
                 MirrorStatus.STATUS_SEEDING,
             ]:
-                msg += f"\n{get_progress_bar_string(download)} {download.progress()}"
+                msg += f"\n├─ {get_progress_bar_string(download)} {download.progress()}"
                 if download.status() == MirrorStatus.STATUS_CLONING:
-                    msg += f"\n<b>Cloned:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n├─♻️ 𝐂𝐥𝐨𝐧𝐞𝐝 ⇢ {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 elif download.status() == MirrorStatus.STATUS_UPLOADING:
-                    msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                    msg += f"\n├─📤 𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 ⇢ {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 else:
-                    msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>Speed:</b> {download.speed()} | <b>ETA:</b> {download.eta()}"
+                    msg += f"\n├─📥 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐝 ⇢ {get_readable_file_size(download.processed_bytes())} of {download.size()}"
+                msg += f"\n├─🚀 𝐒𝐩𝐞𝐞𝐝 ⇢ {download.speed()} | ☀️ 𝗘𝗧𝗔 ⇢ {download.eta()}"
                 try:
-                    msg += f"\n<b>Seeders:</b> {download.aria_download().num_seeders}" \
-                           f" | <b>Peers:</b> {download.aria_download().connections}"
+                    msg += (
+                        f"\n├─🌱 𝐒𝐞𝐞𝐝𝐞𝐫𝐬 ⇢ {download.aria_download().num_seeders}"
+                        f" | ✳️ 𝐏𝐞𝐞𝐫𝐬 ⇢ {download.aria_download().connections}"
+                    )
                 except:
                     pass
                 try:
-                    msg += f"\n<b>Seeders:</b> {download.torrent_info().num_seeds}" \
-                           f" | <b>Leechers:</b> {download.torrent_info().num_leechs}"
+                    msg += (
+                        f"\n├─🌱 𝐒𝐞𝐞𝐝𝐞𝐫𝐬 ⇢ {download.torrent_info().num_seeds}"
+                        f" | 🧲 𝐋𝐞𝐞𝐜𝐡𝐞𝐫𝐬 ⇢ {download.torrent_info().num_leechs}"
+                    )
                 except:
                     pass
-                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n╰─🚫 𝐓𝐨 𝐂𝐚𝐧𝐜𝐞𝐥 ⇢ <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             elif download.status() == MirrorStatus.STATUS_SEEDING:
-                msg += f"\n<b>Size: </b>{download.size()}"
-                msg += f"\n<b>Speed: </b>{get_readable_file_size(download.torrent_info().upspeed)}/s"
-                msg += f" | <b>Uploaded: </b>{get_readable_file_size(download.torrent_info().uploaded)}"
-                msg += f"\n<b>Ratio: </b>{round(download.torrent_info().ratio, 3)}"
-                msg += f" | <b>Time: </b>{get_readable_time(download.torrent_info().seeding_time)}"
-                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                msg += f"\n🗄️ 𝗦𝗶𝘇𝗲 ⇢ {download.size()}"
+                msg += f"\n⚡ 𝗦𝗽𝗲𝗲𝗱 ⇢ {get_readable_file_size(download.torrent_info().upspeed)}/s"
+                msg += f" |⏏️ 𝗨𝗽𝗹𝗼𝗮𝗱𝗲𝗱 ⇢ {get_readable_file_size(download.torrent_info().uploaded)}"
+                msg += f"\n🆎 𝗥𝗮𝘁𝗶𝗼 ⇢ {round(download.torrent_info().ratio, 3)}"
+                msg += f" |⏱️ 𝗧𝗶𝗺𝗲 ⇢ {get_readable_time(download.torrent_info().seeding_time)}"
+                msg += f"\n❌ <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             else:
-                msg += f"\n<b>Size: </b>{download.size()}"
+                msg += f"\n🗂️ 𝗦𝗶𝘇𝗲 ⇢ {download.size()}"
             msg += "\n\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
                 break
-        bmsg = f"<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
-        bmsg += f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {get_readable_time(time() - botStartTime)}"
+        bmsg = f"⚙️ 𝗖𝗣𝗨 ⇢ {cpu_percent()}% | 🧰 𝗙𝗥𝗘𝗘 ⇢ {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+        bmsg += f"\n📏 𝐑𝐀𝐌 ⇢ {virtual_memory().percent}% | 📤 𝗨𝗣𝗧𝗜𝗠𝗘 ⇢ {get_readable_time(time() - botStartTime)}"
         dlspeed_bytes = 0
         upspeed_bytes = 0
         for download in list(download_dict.values()):
             spd = download.speed()
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                if 'K' in spd:
-                    dlspeed_bytes += float(spd.split('K')[0]) * 1024
-                elif 'M' in spd:
-                    dlspeed_bytes += float(spd.split('M')[0]) * 1048576
+                if "K" in spd:
+                    dlspeed_bytes += float(spd.split("K")[0]) * 1024
+                elif "M" in spd:
+                    dlspeed_bytes += float(spd.split("M")[0]) * 1048576
             elif download.status() == MirrorStatus.STATUS_UPLOADING:
-                if 'KB/s' in spd:
-                    upspeed_bytes += float(spd.split('K')[0]) * 1024
-                elif 'MB/s' in spd:
-                    upspeed_bytes += float(spd.split('M')[0]) * 1048576
-        bmsg += f"\n<b>DL:</b> {get_readable_file_size(dlspeed_bytes)}/s | <b>UL:</b> {get_readable_file_size(upspeed_bytes)}/s"
+                if "KB/s" in spd:
+                    upspeed_bytes += float(spd.split("K")[0]) * 1024
+                elif "MB/s" in spd:
+                    upspeed_bytes += float(spd.split("M")[0]) * 1048576
+        bmsg += f"\n📥 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ⇢ {get_readable_file_size(dlspeed_bytes)}/s | 📤 𝐔𝐩𝐥𝐨𝐚𝐝 ⇢ {get_readable_file_size(upspeed_bytes)}/s"
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
-            msg += f"<b>Page:</b> {PAGE_NO}/{pages} | <b>Tasks:</b> {tasks}\n"
+            msg += f"📑 𝗣𝗔𝗚𝗘 ⇢ {PAGE_NO}/{pages} | 📝 𝗧𝗔𝗦𝗞𝗦 ⇢ {tasks}\n"
             buttons = ButtonMaker()
-            buttons.sbutton("Previous", "status pre")
-            buttons.sbutton("Next", "status nex")
+            buttons.sbutton("⬅️ 𝗣𝗥𝗘𝗩𝗜𝗢𝗨𝗦", "status pre")
+            buttons.sbutton("𝗡𝗘𝗫𝗧 ➡️", "status nex")
             button = InlineKeyboardMarkup(buttons.build_menu(2))
             return msg + bmsg, button
         return msg + bmsg, ""
@@ -234,6 +242,14 @@ def is_url(url: str):
 def is_gdrive_link(url: str):
     return "drive.google.com" in url
 
+def is_gdtot_link(url: str):
+    url = re_match(r"https?://(?:\S*\.)?(?:gdtot)\.\S+", url)
+    return bool(url)
+
+def is_appdrive_link(url: str):
+    url = re_match(r"https?://(?:\S*\.)?(?:appdrive|driveapp)\.in/\S+", url)
+    return bool(url)
+
 def is_mega_link(url: str):
     return "mega.nz" in url or "mega.co.nz" in url
 
@@ -275,3 +291,23 @@ def get_content_type(link: str) -> str:
             content_type = None
     return content_type
 
+def secondsToText():
+    secs = AUTO_DELETE_UPLOAD_MESSAGE_DURATION
+    days = secs // 86400
+    hours = (secs - days * 86400) // 3600
+    minutes = (secs - days * 86400 - hours * 3600) // 60
+    seconds = secs - days * 86400 - hours * 3600 - minutes * 60
+    return (
+        ("{0} 𝗱𝗮𝘆{1}, ".format(days, "𝘀" if days != 1 else "") if days else "")
+        + ("{0} 𝗵𝗼𝘂𝗿{1} ".format(hours, "𝘀" if hours != 1 else "") if hours else "")
+        + (
+            "{0} 𝗺𝗶𝗻𝘂𝘁𝗲{1} ".format(minutes, "𝘀" if minutes != 1 else "")
+            if minutes
+            else ""
+        )
+        + (
+            "{0} 𝘀𝗲𝗰𝗼𝗻𝗱{1} ".format(seconds, "𝘀" if seconds != 1 else "")
+            if seconds
+            else ""
+        )
+    )
